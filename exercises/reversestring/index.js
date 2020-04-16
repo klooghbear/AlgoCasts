25df2ecb53ab56049d48660f0129c55c4198ce68
sebastiankloogh@gmail.com <sebastiankloogh@gmail.com>
2020-04-16 14:13:37 +1200

    update reverse string method

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,19 +7,22 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  return str.split('').reduce((rev, char) => char + rev, '')
+  let reversed = '';
+  for(let character of str){
+    reversed = character + reversed;
+  }
+  return reversed
 }
 
-// function reverse(str) {
-//   let reversed = '';
-//   for(let character of str){
-//     reversed = character + reversed;
-//   }
-//   return reversed
-// }
+reverse('asdf');
 
 // function reverse(str) {
 //   return str.split('').reverse().join('')
+// }
+
+// function reverse(str) {
+//   debugger;
+//   return str.split('').reduce((rev, char) => char + rev, '')
 // }
 
 module.exports = reverse;
